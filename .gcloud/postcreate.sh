a6199008626c08133c5ed62038b72d86e54e28f8
James Ward <james@jamesward.com>
2020-01-13 10:30:44 -0700

    fixes

--- .gcloud/postcreate.sh
+++ .gcloud/postcreate.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-which gcloud
-echo $PATH
-
 declare project=$GCP_PROJECT
 declare service=$K_SERVICE
 declare region=$GCP_REGION
@@ -21,12 +18,12 @@ echo "Creating Cloud SQL instance named $instance"
 gcloud sql instances create $instance --database-version=POSTGRES_9_6 --tier=db-f1-micro --region=$region --project=$project --root-password=$db_pass
 
 gcloud beta run services update $service \
-  --add-cloudsql-instances=$service \
-  --set-env-vars=DB_USER=$db_user,DB_PASS=$db_pass,DB_NAME=$db_name,CLOUD_SQL_CONNECTION_NAME=$project:$region:$service \
+  --add-cloudsql-instances=$instance \
+  --set-env-vars=DB_USER=$db_user,DB_PASS=$db_pass,DB_NAME=$db_name,CLOUD_SQL_CONNECTION_NAME=$project:$region:$instance \
   --platform=managed --project=$project --region=$region
 
 wget https://dl.google.com/cloudsql/cloud_sql_proxy.linux.amd64 -O cloud_sql_proxy
 chmod +x cloud_sql_proxy
-./cloud_sql_proxy -instances=$project:$region:$service=tcp:5432 &
+./cloud_sql_proxy -instances=$project:$region:$instance=tcp:5432 &
 sleep 30
 PGPASSWORD=$db_pass psql -h localhost -U $db_user < schema.sql
